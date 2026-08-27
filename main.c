01e47f0aac62d6de298fd5c328d47c122a10c0eb
papadar <aladar@apponyi.net>
2026-08-27 12:24:34 +0000

    updated main to call execute command

--- main.c
+++ main.c
@@ -10,13 +10,19 @@ int main()
 	int state = 0;
 	char *charline;
 	
+	char *ENV = getenv("PATH");
+	extern char **environ;
+
+	/*printf("PATH = %s\n", ENV);*/
+
 	while (state == 0)
 	{
 		printf(PROMPT);
 		charline = read_line();
 		if (charline != NULL)
 		{
-			print_tokens(charline);	
+			/*print_tokens(charline);*/
+			execute_command("/bin/ls", environ, "thishell" );
 		}
 		if (feof(stdin))
 			state = 1;
